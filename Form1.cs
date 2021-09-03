fcf0ab97d4109ac474c9794a29dbdd664d852d93
Daniel Sullivan <theflyingfoool@gmail.com>
2021-09-03 00:47:20 -0500

    Dynamic Steam Game Path Finding

---
+++ Form1.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Globalization;
+using Microsoft.Win32;
+
+namespace N__Assistant
+{
+    public partial class Form1 : Form
+    {
+        //TODO: more robust autodetect added by YupdanielThatsMe#6492
+        string steamGamePath;
+        string profilePath = Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%") + @"\Documents\Metanet\N++";
+        string screenshotsPath = @"\userdata\64929984\760\remote\230270\screenshots";
+        string savePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\N++Assistant";
+
+        BackgroundWorker bgwBackupNow = new BackgroundWorker();
+
+        public Form1()
+        {
+            string steampath = (string)Registry.GetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Valve\\Steam", "InstallPath", "null");
+            if (steampath == "null")
+            {
+                steampath = (string)Registry.GetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\Valve\\Steam", "InstallPath", "null");
+            }
+            if (steampath == "null")
+            {
+                throw new FileNotFoundException("Steam Not Found, WTF Bro is your pc good?");
+            }
+            if (!Directory.Exists(steampath + "\\steamapps\\"))
+            {
+                throw new FileNotFoundException("steamapps Not Found");
+            }
+            screenshotsPath = steampath + screenshotsPath;
+            string[] configLines = File.ReadAllLines(steampath + "\\steamapps\\libraryfolders.vdf");
+            List<string> possiblepaths = new List<string>() { steampath};
+            foreach (string configline in configLines)
+            {
+                if (configline.Contains("\t\t\"path\"\t\t\""))
+                {
+                    Console.WriteLine(configline.Replace("\t\t\"path\"\t\t\"", "").Replace("\\\\", "\\").Replace("\"", ""));
+                }
+            }
+            foreach(string possiblepath in possiblepaths)
+            {
+                if (Directory.Exists(possiblepath + "\\steamapps\\common\\N++") )
+                {
+                    steamGamePath = possiblepath + "\\steamapps\\common\\N++";
+                    break;
+                }
+            }
+            if (steamGamePath == "")
+            {
+                throw new FileNotFoundException("N++ not installed in steam");
+            }
+            InitializeComponent();
+
+
+            // rename linked labels
+            steamInstallDir.Text = steamGamePath;
+            profileDir.Text = profilePath;
+            screenshotsDir.Text = screenshotsPath;
+            backupsDir.Text = savePath;
+
+            // create backup directory if it doesnt exist
+            if (!Directory.Exists(savePath)) Directory.CreateDirectory(savePath);
+            if (!Directory.Exists(savePath + @"\Profiles")) Directory.CreateDirectory(savePath + @"\Profiles");
+            if (!Directory.Exists(savePath + @"\Sounds")) Directory.CreateDirectory(savePath + @"\Sounds");
+            if (!Directory.Exists(savePath + @"\Replays")) Directory.CreateDirectory(savePath + @"\Replays");
+            if (!Directory.Exists(savePath + @"\Levels")) Directory.CreateDirectory(savePath + @"\Levels");
+            if (!Directory.Exists(savePath + @"\EditorLevels")) Directory.CreateDirectory(savePath + @"\EditorLevels");
+            if (!Directory.Exists(savePath + @"\Palettes")) Directory.CreateDirectory(savePath + @"\Palettes");
+            if (!Directory.Exists(savePath + @"\GameLevels")) Directory.CreateDirectory(savePath + @"\GameLevels");
+
+            // create palettes directory in game if it doesnt exist (it's needed to place custom palettes)
+            if (!Directory.Exists(steamGamePath + @"\NPP\Palettes")) Directory.CreateDirectory(steamGamePath + @"\NPP\Palettes");
+
+            // mark backup now checklist items as checked by default
+            //TODO: save user settings and reload the same on launch
+            // default should probably be just profile and editor levels, not everything
+            /*for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                checkedListBox1.SetItemChecked(i, true);
+            }*/
+            // set checkbox on profile backup default on
+            checkedListBox1.SetItemChecked(0, true);
+            // set checkbox on level editor backup default on
+            checkedListBox1.SetItemChecked(2, true);
+
+            // initialize background worker for backup now button
+            bgwBackupNow.DoWork += new DoWorkEventHandler(bgwBackupNow_DoWork);
+            bgwBackupNow.ProgressChanged += new ProgressChangedEventHandler(bgwBackupNow_ProgressChanged);
+            bgwBackupNow.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bgwBackupNow_RunWorkerCompleted);
+            bgwBackupNow.WorkerReportsProgress = true;
+
+            tabControl1.Selecting += new TabControlCancelEventHandler(tabControl1_Selecting);
+
+            /*Process[] processCollection = Process.GetProcesses();
+            foreach (Process p in processCollection)
+            {
+                Console.WriteLine(p.ProcessName); // N++
+            }*/
+
+            loadProfile.Enabled = false;
+            deleteProfile.Enabled = false;
+        }
+
+        void tabControl1_Selecting(object sender, TabControlCancelEventArgs e)
+        {
+            TabPage current = (sender as TabControl).SelectedTab;
+
+            // switch to profile tab
+            if (current == tabPage2)
+            {
+                profileList.Items.Clear();
+                PopulateListBox(profileList, savePath + @"\Profiles", "*.zip");
+                loadProfile.Enabled = false;
+                deleteProfile.Enabled = false;
+            }
+
+            // switch to soundpacks tab
+            if (current == tabPage3)
+            {
+                // spreadsheet of new sound packs
+                // https://docs.google.com/spreadsheets/d/18PshamVuDNyH396a7U3YDFQmCw18s4gIVZ_WrFODRd4/edit#gid=0
+
+                // latest google sheets api example code
+                // https://github.com/popcron/sheets
+            }
+
+            // switch to palettes tab
+            if (current == tabPage4)
+            {
+                // all official palettes
+                // https://cdn.discordapp.com/attachments/197793786389200896/592821804746276864/Palettes.zip
+
+                // spreadsheet of new palettes
+                // https://docs.google.com/spreadsheets/d/1I2f87Qhfs6rxzZq5dQRDbLKYyaGLqTdCkLqfNfrw1Mk/edit#gid=0
+
+                // allpalettes.zip (supposedly contains all community palettes, updated whenever)
+                // https://drive.google.com/file/d/1Ly3g_4VKcMsTLwXJpY3jPk-IlAGZ5RJG/view?usp=sharing
+            }
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (Directory.Exists(steamGamePath))
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    Arguments = steamGamePath,
+                    FileName = "explorer.exe"
+                };
+
+                Process.Start(startInfo);
+            }
+            else
+            {
+                MessageBox.Show(string.Format("{0} Directory does not exist!", steamGamePath));
+            }
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (Directory.Exists(profilePath))
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    Arguments = profilePath,
+                    FileName = "explorer.exe"
+                };
+
+                Process.Start(startInfo);
+            }
+            else
+            {
+                MessageBox.Show(string.Format("{0} Directory does not exist!", profilePath));
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (DetectNPPRunning() == true)
+            {
+                MessageBox.Show("Please close N++ before doing backup");
+                return;
+            }
+            backupNow.Enabled = false;
+            progressBar1.Show();
+            bgwBackupNow.RunWorkerAsync();
+        }
+
+        void bgwBackupNow_DoWork(object sender, DoWorkEventArgs e)
+        {
+            bgwBackupNow.ReportProgress(0, "Zipping nprofile");
+            if (checkedListBox1.GetItemCheckState(0) == CheckState.Checked)
+            {
+                // backup profile
+                using (FileStream fs = new FileStream(savePath + @"\Profiles\nprofile" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".zip", FileMode.Create))
+                using (ZipArchive arch = new ZipArchive(fs, ZipArchiveMode.Create))
+                {
+                    arch.CreateEntryFromFile(profilePath + @"\nprofile", "nprofile");
+                }
+            }
+            bgwBackupNow.ReportProgress(15,"Zipping Soundpack");
+            if (checkedListBox1.GetItemCheckState(1) == CheckState.Checked)
+            {
+                // backup soundpack
+                ZipFile.CreateFromDirectory(steamGamePath + @"\NPP\Sounds", savePath + @"\Sounds\Sounds" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".zip");
+            }
+            bgwBackupNow.ReportProgress(30, "Zipping Editor Levels");
+            if (checkedListBox1.GetItemCheckState(2) == CheckState.Checked)
+            {
+                // backup editor levels
+                ZipFile.CreateFromDirectory(profilePath + @"\levels", savePath + @"\EditorLevels\EditorLevels" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".zip");
+            }
+            bgwBackupNow.ReportProgress(40, "Zipping Replays (attract files)");
+            if (checkedListBox1.GetItemCheckState(3) == CheckState.Checked)
+            {
+                // backup replays (attract files)
+                ZipFile.CreateFromDirectory(profilePath + @"\attract", savePath + @"\Replays\Replays" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".zip");
+            }
+            bgwBackupNow.ReportProgress(60, "Zipping Palettes");
+            if (checkedListBox1.GetItemCheckState(4) == CheckState.Checked)
+            {
+                // backup palettes
+                ZipFile.CreateFromDirectory(steamGamePath + @"\NPP\Palettes", savePath + @"\Palettes\Palettes" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".zip");
+            }
+            bgwBackupNow.ReportProgress(80, "Zipping Game Levels");
+            if (checkedListBox1.GetItemCheckState(5) == CheckState.Checked)
+            {
+                // backup gamelevels
+                ZipFile.CreateFromDirectory(steamGamePath + @"\NPP\Levels", savePath + @"\GameLevels\GameLevels" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".zip");
+            }
+            bgwBackupNow.ReportProgress(100, "Done with backup!");
+
+        }
+
+        void bgwBackupNow_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            progressLabel.Text = String.Format("{0}", e.UserState);
+            //label2.Text = String.Format("Total items transfered: {0}", e.UserState);
+        }
+
+        void bgwBackupNow_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            progressLabel.Text = "Done with backup!";
+            progressBar1.Hide();
+            backupNow.Enabled = true;
+        }
+        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (Directory.Exists(savePath))
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    Arguments = savePath,
+                    FileName = "explorer.exe"
+                };
+
+                Process.Start(startInfo);
+            }
+            else
+            {
+                MessageBox.Show(string.Format("{0} Directory does not exist!", savePath));
+            }
+        }
+
+        private void linkLabel4_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (Directory.Exists(screenshotsPath))
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    Arguments = screenshotsPath,
+                    FileName = "explorer.exe"
+                };
+
+                Process.Start(startInfo);
+            }
+            else
+            {
+                MessageBox.Show(string.Format("{0} Directory does not exist!", screenshotsPath));
+            }
+        }
+
+        private void PopulateListBox(ListBox lsb, string Folder, string FileType)
+        {
+            DirectoryInfo dinfo = new DirectoryInfo(Folder);
+            FileInfo[] Files = dinfo.GetFiles(FileType);
+            foreach (FileInfo file in Files)
+            {
+                lsb.Items.Add(file.Name + " (" + file.Length/1024 + "Kb)");
+            }
+        }
+
+        private bool DetectNPPRunning()
+        {
+            Process[] pname = Process.GetProcessesByName("N++");
+            if (pname.Length == 0)
+                return false;
+            else
+                return true;
+        }
+
+        private void backupProfile_Click(object sender, EventArgs e)
+        {
+            profileBackupLabel.Text = "Profile backup started!";
+            // backup profile
+            using (FileStream fs = new FileStream(savePath + @"\Profiles\nprofile" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".zip", FileMode.Create))
+            using (ZipArchive arch = new ZipArchive(fs, ZipArchiveMode.Create))
+            {
+                arch.CreateEntryFromFile(profilePath + @"\nprofile", "nprofile");
+            }
+            profileBackupLabel.Text = "Profile backup completed!";
+            
+            profileList.Items.Clear();
+            PopulateListBox(profileList, savePath + @"\Profiles", "*.zip");
+        }
+
+        private void profileList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            loadProfile.Enabled = true;
+            deleteProfile.Enabled = true;
+        }
+
+        private void deleteProfile_Click(object sender, EventArgs e)
+        {
+            try {
+                File.Delete(savePath + @"\Profiles\" + profileList.SelectedItem.ToString().Split(' ')[0]);
+                profileList.Items.Remove(profileList.SelectedItem);
+                loadProfile.Enabled = false;
+                deleteProfile.Enabled = false;
+            } catch (Exception exc) {
+                MessageBox.Show(exc.Message);
+            }
+            
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+# NPlusPlusAssistant
+
+N++ Assistant helps with backups and installing community mod stuff for the videogame [N++](https://store.steampowered.com/app/230270/N_NPLUSPLUS/). Only for the Steam Windows version.
+
+Needs Visual Studio Community 2019 with C# .Net stuff installed to compile from source.
+
+## Stuff it already does:
+
+* autodetect main folders (list them, with hot link to go there)
+
+* backup profile, editor levels, replays, sounds, palettes and game levels
+
+* detect if npp is running
+
+* list all profiles on backup and select one to replace current one or delete
+
+## Stuff it'll eventually do:
+
+* download soundpacks and select one to replace the current one
+
+* download palettes and select which ones to install
+
+* download metanet maps pack and select one to put on your map levels editor folder (for practice / remixes)
+
+* install/revert community map packs
+
+* edit nppconf (useful for troubleshooting some launch problems)
+
+* have an n++ icon
+
+* display nprofile road to 100% completion details / corruption checks
